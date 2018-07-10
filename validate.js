c2922c1f858a2120826cc911cec02eb25180afa7
Rene Koch <renekoch@hotmail.com>
2018-07-10 16:37:48 +0200

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,43 +1,6 @@
 define(['jquery'], function ($, helpers) {
 
 	/**
-	 *  # Rule defination
-	 *
-	 *  string: "<rulename>:<param1>:<param2>:<paramX>;<error message>"
-	 *  object: {rule: "<rulename>", params: [<param1>, <param2>, <paramX>], msg: "<error message>"}
-	 *  function: function(input: <value to check>, params: [<param1>, <param2>, <paramX>], msg: "<error message>")
-	 *
-	 *
-	 *  # Usage
-	 *
-	 *  ## Event hook
-	 *
-	 *  $.validate(
-	 *    <CSS selector>,    - which elements to hook on to. DEFAULT: [data-validate],
-	 *    <rules>            - array of rules, *optional*. DEFAULT: element attribute data-validate
-	 *  )
-	 *
-	 *
-	 *  ## For single element
-	 *
-	 *  return array of errors messages, if empty array then <value> is valid
-	 *
-	 *  $(<CSS selector>).validate(
-	 *    <rules>            - array of rules, *optional*. DEFAULT: element attribute data-validate
-	 *  )
-	 *
-	 *
-	 *  ## Plain JS
-	 *
-	 *  return array of errors messages, if empty array then <value> is valid
-	 *
-	 *  validate(
-	 *    <value>,           - value to check
-	 *    <rules>            - array of rules
-	 *  )
-	 */
-
-	/**
 	 * @typedef {Object} ValidateRule
 	 * @property {string|function} rule
 	 * @property {array} [params]
